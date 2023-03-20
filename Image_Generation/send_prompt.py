c5e6f21fe955513f7de85cedec3348ad19591292
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2023-03-20 15:37:34 +0200

    Added a function for creating a dictionary of all the URL's for the services we use

--- Image_Generation/send_prompt.py
+++ Image_Generation/send_prompt.py
@@ -6,7 +6,7 @@ from io import BytesIO
 import argparse
 from my_secrets import API_KEY
 URL = ""
-
+URLS = {} # dictionary of urls for each service (whisper, sd, etc)
 
 def get_url():
     global URL
@@ -18,6 +18,44 @@ def get_url():
     if len(response.json()['endpoints'])==0:
         raise Exception(f'No endpoints found')
     URL = response.json()['endpoints'][0]['public_url']
+    
+'''
+A method to populate the URLS dictionary with the urls for each service, so that we can use them later
+'''
+def get_service_urls():
+    global URLS
+    api_url = "https://api.ngrok.com/tunnels"
+    headers = {'Authorization': f'Bearer {API_KEY}', 'Ngrok-Version': '2'}
+    response = requests.get(api_url,headers=headers)
+    if response.status_code != 200:
+        raise Exception(f'API request failed: {response.text}')
+
+    response = response.json()
+    ic(response)
+    
+    tunnels = {}
+    # iterate through the tunnels and get the public url, save it in the tunnels dictionary as a value, where the key is the tunnel session id
+    for tunnel in response['tunnels']:
+        tunnels[tunnel['tunnel_session']['id']]=(tunnel['public_url'])
+        
+    # a dictionary of credential ids and the corresponding  service name
+    credential_id = {'cr_2NFNS09sQ2z2nMSTrIkn5ZsMz80':'whisper',
+                     'cr_2Ava69iIPmwypV1AyMlXHJ0MMvK':'sd'}
+    
+    api_url = "https://api.ngrok.com/tunnel_sessions"
+    headers = {'Authorization': f'Bearer {API_KEY}', 'Ngrok-Version': '2'}
+    response = requests.get(api_url,headers=headers)
+    if response.status_code != 200:
+        raise Exception(f'API request failed: {response.text}')
+    tunnel_sessions = {}
+    # match the tunnel session id with the credential id to use in locating the service name
+    for tunnel in response.json()['tunnel_sessions']:
+        tunnel_sessions[tunnel['id']]=(tunnel['credential']['id'])
+    ic(tunnel_sessions)
+    # iterate through the tunnel sessions and add the corresponding url to the URLS dictionary
+    for t in tunnel_sessions.keys():
+        URLS[credential_id[tunnel_sessions[t]]] = tunnels[t]
+    ic(URLS)
  
 def check_style_api():
     response = requests.get(URL+'/sdapi/v1/prompt-styles')
@@ -105,6 +143,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", type=str, default="", help="url for the api server")
     args = parser.parse_args()
+
     if args.url != "":
         URL = args.url
     else:
