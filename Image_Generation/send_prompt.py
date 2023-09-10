bd790ab19b5ee88858415ca53b3fa257d0feb7db
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2023-09-10 12:06:15 +0300

    Some cleanup before merge to main

--- Image_Generation/send_prompt.py
+++ Image_Generation/send_prompt.py
@@ -46,47 +46,6 @@ def get_url():
     URL = response.json()["endpoints"][0]["public_url"]
     return URL
 
-
-# '''
-# A method to populate the URLS dictionary with the urls for each service, so that we can use them later
-# '''
-# def get_service_urls():
-# ic.disable()
-# global URLS
-# api_url = "https://api.ngrok.com/tunnels"
-# headers = {'Authorization': f'Bearer {API_KEY}', 'Ngrok-Version': '2'}
-# response = requests.get(api_url, headers=headers)
-# if response.status_code != 200:
-#     raise Exception(f'API request failed: {response.text}')
-
-# response = response.json()
-# ic(response)
-# tunnels = {}
-# # iterate through the tunnels and get the public url, save it in the tunnels dictionary as a value, where the key is the tunnel session id
-# for tunnel in response['tunnels']:
-#     tunnels[tunnel['tunnel_session']['id']] = (tunnel['public_url'])
-
-# # a dictionary of credential ids and the corresponding  service name
-# credential_id = {'cr_2NFNS09sQ2z2nMSTrIkn5ZsMz80': 'whisper',
-#                  'cr_2Ava69iIPmwypV1AyMlXHJ0MMvK': 'sd'}
-
-# api_url = "https://api.ngrok.com/tunnel_sessions"
-# headers = {'Authorization': f'Bearer {API_KEY}', 'Ngrok-Version': '2'}
-# response = requests.get(api_url, headers=headers)
-# if response.status_code != 200:
-#     raise Exception(f'API request failed: {response.text}')
-# tunnel_sessions = {}
-# # match the tunnel session id with the credential id to use in locating the service name
-# for tunnel in response.json()['tunnel_sessions']:
-#     tunnel_sessions[tunnel['id']] = (tunnel['credential']['id'])
-# ic(tunnel_sessions)
-# # iterate through the tunnel sessions and add the corresponding url to the URLS dictionary
-# for t in tunnel_sessions.keys():
-#     URLS[credential_id[tunnel_sessions[t]]] = tunnels[t]
-# ic(URLS)
-# return URLS
-
-
 # TODO: check edge cases
 def poll_results():
     # s.auth = (USERNAME, PASSWORD)
@@ -226,7 +185,7 @@ def send_to_sd(prompt,isWeb = False):
 
             return f".\{date_folder}\image_{counter}_{now}.png"
         else:
-            ic("Image completley black!")
+            ic("Image completely black!")
 
 
 if __name__ == "__main__":
