05e206d5ef76ec50a8b569a102a62f0a37b9ba7a
Daniel <funkey7dan@gmail.com>
2023-09-28 10:59:01 +0300

    Added comments

--- WebGui/app.py
+++ WebGui/app.py
@@ -3,17 +3,10 @@
 import eventlet
 
 eventlet.monkey_patch()
+
 import sys
 import time
-from flask import (
-    request,
-    Flask,
-    render_template,
-    session,
-    jsonify,
-    redirect,
-    url_for
-)
+from flask import request, Flask, render_template, session, jsonify, redirect, url_for
 from flask_sse import sse
 from flask_session import Session
 import requests
@@ -41,8 +34,11 @@ from gpt_call import call_openai
 from send_prompt import send_to_sd
 from utils import get_service_urls
 from send_prompt import poll_results
-#from my_secrets_web import MGDB_PASS
+
+# from my_secrets_web import MGDB_PASS
 MGDB_PASS = os.environ.get("MGDB_PASS")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 IS_TEST = False
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
@@ -64,25 +60,45 @@ app.config["SECRET_KEY"] = "secret!"
 app.register_blueprint(sse, url_prefix="/stream")
 Session(app)
 
+
 def get_database():
+    """
+    Get the database connection.
+
+    This function creates a connection to the MongoDB database using the provided
+    CONNECTION_STRING. It returns a MongoClient object that can be used to interact
+    with the database.
+    MGDB_PASS is an environment variable that contains the password for the database.
+
+    Parameters:
+        None
+
+    Returns:
+    pymongo.database.Database: The MongoDB database object.
+
+    """
     # Create a connection
-    CONNECTION_STRING = f"mongodb+srv://BenEliz:{MGDB_PASS}@cluster0.q59ddrd.mongodb.net"
+    CONNECTION_STRING = (
+        f"mongodb+srv://BenEliz:{MGDB_PASS}@cluster0.q59ddrd.mongodb.net"
+    )
     client = MongoClient(CONNECTION_STRING)
-    return client['DreamGenie'] # Replace with your database name
+    return client["DreamGenie"]  # Replace with your database name
+
 
 dbname = get_database()
-# Your collection, for example, 'feedback'
+
 feedback_collection = dbname["Feedback"]
+
 # Global variables
 progress1 = 0
 scenes_list = []
 current_scene_index = 0
 URLS = get_service_urls()
-if URLS['sd'] == '' or URLS['whisper'] == '':
+if URLS["sd"] == "" or URLS["whisper"] == "":
     raise RuntimeError("The dsi server is down!")
 
 
-#currently not used
+# currently not used
 def poll_results_until_done():
     time_started = time.time()
     TIMEOUT = 50  # seconds
@@ -96,13 +112,21 @@ def poll_results_until_done():
 
 
 @app.route("/")
-def index():
+def index() -> str:
+    """
+    Function that handles the index route of the application.
+
+    Parameters:
+        None
+    Returns:
+        str: The rendered HTML template, with the user id passed to it in context.
+    """
     # generate a unique user id
     if "user_id" not in session:
         user_id = str(uuid.uuid4())
         session["user_id"] = user_id
         session.modified = True
-        app.logger.info("User id " + user_id+" added to session")
+        app.logger.info("User id " + user_id + " added to session")
     else:
         user_id = session["user_id"]
     app.logger.info("User id: " + user_id)
@@ -110,7 +134,19 @@ def index():
 
 
 @app.route("/gallery")
-def gallery():
+def gallery() -> str:
+    """
+    Renders the gallery page if the user is authenticated, otherwise redirects to the home page.
+
+    Parameters:
+        None
+
+    Returns:
+        - If the user is not authenticated:
+            - A string message indicating that no user ID was found.
+        - If the user is authenticated:
+            - The rendered template for the gallery page with the user ID passed as a parameter.
+    """
     if "user_id" not in session:
         return "No user id found. Please go back to the home page and try again."
     else:
@@ -123,7 +159,13 @@ def gallery():
 @app.route("/user_input", methods=["POST"])
 def receive_user_input():
     """
-    Endpoint to receive a POST request with user input.
+    Receive user input from a POST request and start processing it in the background.
+
+    Parameters:
+    None
+
+    Returns:
+        JSON response indicating the success of the operation.
     """
     global progress, scenes_list, current_scene_index
     input_data = request.form["user_input"]
@@ -137,24 +179,34 @@ def receive_user_input():
     with app.app_context():
         eventlet.spawn(process_input, input_data, session["user_id"])
     app.logger.info("Started background task")
+    # TODO: add handling for errors
     return jsonify(success=True)
 
 
 def process_input(input_data, user_id):
-    global progress1, scenes_list
+    """
+    Process input data and generate images for each scene in the input data.
 
-    # TODO: change the placeholder!
+    Args:
+        input_data (str): The input data to be processed.
+        user_id (str): The ID of the user. Passed because the function is called in a separate thread, outside of the request context.
+
+    Returns:
+        None
+    """
+    global progress1, scenes_list
 
     # Call OpenAI GPT-3 to separate scenes
     scenes_list = call_openai(input_data, test=IS_TEST)
+
+    # If no scenes were returned, emit an error message to the client
     if len(scenes_list) == 0:
         app.logger.error("len(scenes_list) == 0")
         sse.publish(
-            {"message": 'error'},
+            {"message": "error"},
             type="message",
             channel=user_id,
         )
-    # scenes_list = [input_data]
 
     # Get today's date
     today = datetime.date.today()
@@ -197,18 +249,24 @@ def process_input(input_data, user_id):
         app.logger.info(f"Emitted: Image path: {relative_image_path}")
     with app.app_context():
         sse.publish(
-            {"message": 'finished'},
+            {"message": "finished"},
             type="message",
             channel=user_id,
         )
-        app.logger.info('Published finished message')
-
-
-# Create an empty list to store received chunks
-audio_chunks = []
+        app.logger.info("Published finished message")
 
 
 def send_request(audio_data, user_id):
+    """
+    Sends a request to the whisper service endpoint with the provided audio data and user ID, in order to generate text from the audio.
+
+    Parameters:
+        audio_data (bytes): The audio data to be sent in the request, received from the client.
+        user_id (str): The ID of the user, passed because the function is called in a separate thread, outside of the request context.
+
+    Returns:
+        None
+    """
     url = URLS["whisper"] + "/whisper"
     # Wraps the audio data in a BytesIO object to mimic a file
     audio_file = BytesIO(audio_data)
@@ -221,68 +279,98 @@ def send_request(audio_data, user_id):
     # The 'files' parameter for 'requests.post' should be a dictionary or a list of tuples
     files = {"file": audio_file}
     app.logger.info(files)
-    response = requests.post(url, files=files, auth=("bdt", "12xmnxqgkpzj9cjb"))
+    # Send the request to the whisper service endpoint using HTTP Basic Auth
+    response = requests.post(url, files=files, auth=(USERNAME, PASSWORD))
     app.logger.info(response)
+    # TODO: add handling for errors
     result = response.json()["results"][0]["transcript"]
     print(result)
     app.logger.info(result)
 
-    # Emitting to the originating client
+    # Emitting the transcribed text to the originating client
     with app.app_context():
         sse.publish(
-            {"audio_result": result},
-            type="audio_result",
-            channel=user_id,
-            retry=1000
+            {"audio_result": result}, type="audio_result", channel=user_id, retry=1000
         )
     app.logger.info("Emitted audio result to " + user_id)
 
 
 @app.route("/audio", methods=["POST"])
 def process_audio():
+    """
+    Process the received audio file.
+
+    This function is responsible for processing the audio file that is received
+    via a POST request to the "/audio" endpoint. The function expects the audio
+    file to be sent as part of the request's payload.
+    The function then calls the "send_request" function in a separate thread, in order to pass the audio data to the whisper service endpoint for transcription.
+
+    Parameters:
+    - None
+
+    Returns:
+    - A JSON response containing the success status of the audio processing.
+      The response is in the form of a dictionary with a single key "success"
+      which holds a boolean value indicating whether the processing was
+      successful or not.
+    """
     audioData = request.files["audio"]
     app.logger.info("Audio received")
     app.logger.info(audioData)
     try:
-        # decoded_data = base64.b64decode(audioData.read())
         with app.app_context():
             eventlet.spawn(send_request, audioData.read(), session["user_id"])
         success = True
     except Exception as e:
         app.logger.error(e)
         success = False
-    return jsonify(success)    
+    return jsonify(success)
 
-@app.route('/feedback')
+
+@app.route("/feedback")
 def feedback():
-    return render_template('feedback.html')
+    """
+    A function that handles the "/feedback" route, showing the user a feedback form.
 
-@app.route('/submit_feedback', methods=['POST'])
+    Returns:
+        The rendered "feedback.html" template.
+    """
+    return render_template("feedback.html")
+
+
+@app.route("/submit_feedback", methods=["POST"])
 def submit_feedback():
-    image_rating = request.form.get('image_rating')
-    scene_rating = request.form.get('scene_rating')
-    experience_rating = request.form.get('experience_rating')
-    
-    # New fields
-    age = request.form.get('age')
-    gender = request.form.get('gender')
-    familiarity = request.form.get('familiarity')
-    comments = request.form.get('comments')
-    
-    # Prepare the document to insert
+    """
+    The function that handles the "/submit_feedback" route, receiving the feedback data from the feedback form and inserting it into the MongoDB database.
+
+    Parameters:
+        None
+
+    Returns:
+        The rendered template for the "thank_you.html" page.
+    """
+    # Get the feedback data from the form
+    image_rating = request.form.get("image_rating")
+    scene_rating = request.form.get("scene_rating")
+    experience_rating = request.form.get("experience_rating")
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    familiarity = request.form.get("familiarity")
+    comments = request.form.get("comments")
+
+    # Prepare the data to insert
     feedback_data = {
         "user_id": session["user_id"],
         "image_rating": int(image_rating),
         "scene_rating": int(scene_rating),
         "experience_rating": int(experience_rating),
-        "age": int(age) if age else None, 
+        "age": int(age) if age else None,
         "gender": gender,
         "familiarity": familiarity,
-        "comments": comments
+        "comments": comments,
     }
-    
+
     # Insert into MongoDB
     feedback_collection.insert_one(feedback_data)
-    
-    return render_template('thank_you.html')
-    
+
+    return render_template("thank_you.html")
